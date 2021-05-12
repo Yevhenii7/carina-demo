c93ab3fb6c96123274937db270224dade8e69320
yevhenii <ykolchyba@solvd.com>
2021-05-12 13:29:34 +0300

    add api tests

---
+++ src/main/java/com/qaprosoft/carina/demo/api/gorest/co/in/GetUser.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013-2021 QAPROSOFT (http://qaprosoft.com/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qaprosoft.carina.demo.api.gorest.co.in;
+
+import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+import com.qaprosoft.carina.core.foundation.utils.Configuration;
+
+import java.util.Properties;
+
+public class GetUser extends AbstractApiMethodV2 {
+
+    public GetUser() {
+        super(null, "api.gorest.co.in/_get/rs.json", new Properties());
+        replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
+        request.header("Authorization", "Bearer 7614ee19b13d9a72373f807db0d691a910708f31398c0ccb9ea7989cd544b2bd");
+
+    }
+}
